2c77505c275f1559368f3c139ca005d1a2d48e43
mahin33ferdous <mahinferdousshoily@gmail.com>
2023-06-06 02:49:09 -1100

    product display

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -3,11 +3,21 @@ import './Product.css'
 
 const Product = (props) => {
     console.log(props.product)
-    const{name,price,img,seller,rating}=props.product;
+    const{name,price,img,seller,ratings}=props.product;
     return (
         <div className='product'>
            <img src={img}/>
+           <div className='pinfo'>
+
            <p className='pname'>{name}</p>
+           <p>Price={price}</p>
+           <p><small>Seller: {seller}</small></p>
+           <p><small>rating: {ratings}stars</small></p>
+
+           </div>
+           <button className='btn'>
+            <p>Add to cart</p>
+           </button>
         </div>
     );
 };
